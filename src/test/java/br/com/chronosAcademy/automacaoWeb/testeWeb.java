75f5be2911a12475cabc4987413ce1f29c0a7907
LeandroMarHUB <79597874+LeandroMarHUB@users.noreply.github.com>
2023-01-26 17:18:13 -0300

    aula 06 anotações

--- src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,14 +11,25 @@ import static org.junit.Assert.*;
 
 public class testeWeb {
 
-    @Test
-    public void primeiroTeste(){
+
+
+    ChromeDriver driver;
+
+    @Before
+    public void inicializaTeste(){
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
+
+
+
+    }
+
+    @Test
+    public void primeiroTeste(){
+
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
@@ -25,6 +38,13 @@ public class testeWeb {
         driver.quit();
 
     }
+    @After
+    public void finalizaTeste(){
+        driver.quit();
+    }
+
+
+
 
 
 
